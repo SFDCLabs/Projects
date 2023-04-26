2f63ad9b042e03da27dbc264c2740be41f84a082
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2023-04-27 03:47:41 +0530

    signme

---
+++ bplm/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+main_menu() {
+    case $1 in 
+        CLEAR_DOCS)
+            echo "inside clear docs"
+            # rm -rf docs/*
+            ;;
+
+        DOCS)
+            echo "inside docs"
+            # java -jar lib/apex.jar -s "sfdx-src/main/default/classes" -t "docs/"
+            ;;
+        *)
+            echo "inside default"
+            help_menu
+            ;;
+    esac
+}
+
+
+help_menu() {
+    echo "help"
+}
+
+main_menu $1
