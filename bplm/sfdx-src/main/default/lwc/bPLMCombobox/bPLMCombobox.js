fb93fcac13b924d84c2a8cdd0232a5107d36eb04
rahul chaudhary <rahulchaudharyofficial@outlook.com>
2023-04-28 07:15:14 +0530

    latest changes

--- bplm/sfdx-src/main/default/lwc/bPLMCombobox/bPLMCombobox.js
+++ bplm/sfdx-src/main/default/lwc/bPLMCombobox/bPLMCombobox.js
@@ -3,13 +3,19 @@ import { LightningElement, api } from 'lwc';
 export default class BPLMCombobox extends LightningElement {
     @api isMultiSelect = false;
     value = 'inProgress';
+    options = []
+    values = []
+    requiredOptions = [];
 
-    get options() {
-        return [
+    connectedCallback() {
+        const items = [
             { label: 'New', value: 'new' },
             { label: 'In Progress', value: 'inProgress' },
             { label: 'Finished', value: 'finished' },
         ];
+        this.options.push(...items);
+        this.values.push(...[this.value]);
+        this.requiredOptions.push(...[this.value]);
     }
 
     handleChange(event) {
